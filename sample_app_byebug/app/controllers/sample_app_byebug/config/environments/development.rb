06ef09ad80fbc83cd904bc89776ba622e3946396
Your Name <183378925+NT-lang-png@users.noreply.github.com>
2024-10-12 07:40:00 +0000

    [finish] dyebug

--- sample_app_byebug/app/controllers/sample_app_byebug/config/environments/development.rb
+++ sample_app_byebug/app/controllers/sample_app_byebug/config/environments/development.rb
@@ -73,9 +73,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
-  
-  config.hosts << "~.vfs.cloud9.us-east-1.amazonaws.com"
-  config.hosts << "e4f30599b83c459f86f2212b4cf06567.vfs.cloud9.ap-northeast-1.amazonaws.com"
-
 end
